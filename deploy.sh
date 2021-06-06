9d9aae2bd630f30c923fc85ab4a520daae0081ed
Arszilla <arszilla@arszilla.com>
2021-06-06 14:24:08 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,12 +19,12 @@ do
         if [ "$DEVICE" -eq 1 ]
         then
             # Move the laptop specific files to /home/$SUDO_USER/:
-            cp -r ./"Laptop Specific"/etc/skel/. /home/$SUDO_USER/
+            /usr/bin/cp -r ./"Laptop Specific"/etc/skel/. /home/$SUDO_USER/
             
         elif [ "$DEVICE" -eq 2 ]
         then
             # Move the laptop specific files to /home/$SUDO_USER/:
-            cp -r ./"Desktop Specific"/etc/skel/. /home/$SUDO_USER/
+            /usr/bin/cp -r ./"Desktop Specific"/etc/skel/. /home/$SUDO_USER/
             
         fi
 
@@ -38,19 +38,19 @@ do
 done
 
 # oh-my-zsh related actions:
-sudo -H -u $SUDO_USER sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+/usr/bin/sudo -H -u $SUDO_USER sh -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # oh-my-zsh autosuggestions plugin installation:
-sudo -H -u $SUDO_USER git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/$SUDO_USER/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+/usr/bin/sudo -H -u $SUDO_USER /usr/bin/git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/$SUDO_USER/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # oh-my-zsh completions plugin installation:
-sudo -H -u $SUDO_USER git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-/home/$SUDO_USER/.oh-my-zsh/custom}/plugins/zsh-completions
+/usr/bin/sudo -H -u $SUDO_USER /usr/bin/git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-/home/$SUDO_USER/.oh-my-zsh/custom}/plugins/zsh-completions
 
 # Add the dotfiles to the /home/$SUDO_USER/:
-cp -r ./etc/skel/. /home/$SUDO_USER/
+/usr/bin/cp -r ./etc/skel/. /home/$SUDO_USER/
 
 # Replace SSH's config at /etc/ssh/:
-cp -r ./etc/ssh/ssh_config /etc/ssh/
+/usr/bin/cp -r ./etc/ssh/ssh_config /etc/ssh/
 
 # Set the permissions for the SSH private and public keys:
 for FILE in /home/$SUDO_USER/.ssh/*
@@ -59,7 +59,7 @@ do
     # Check if $FILE is README.md:
     if [ "$FILE" == "README.md" ]
     then
-        rm $FILE
+        /usr/bin/rm $FILE
     
     # Check if $FILE is known_hosts:
     elif [ "$FILE" == "known_hosts" ]
@@ -69,11 +69,11 @@ do
     # Check if $FILE is a public key:
     elif [ "${FILE: -4}" == ".pub" ]
     then
-        chmod 644 $FILE
+        /usr/bin/chmod 644 $FILE
 
     # If $FILE is a private key:
     else
-        chmod 600 $FILE
+        /usr/bin/chmod 600 $FILE
 
     fi
 done
