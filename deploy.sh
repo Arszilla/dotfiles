433dee86413a73c277ac39930dbe0dc686d1ac8e
Arszilla <arszilla@arszilla.com>
2022-10-22 18:02:20 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-help_menu() {
-    echo "Usage: $0 [-d/-l] [-h]" >&2
-}
-
 user_dirs() {
     # Make sure user-dirs are created for $USER:
     /usr/bin/xdg-user-dirs-update
@@ -12,38 +8,9 @@ user_dirs() {
     /usr/bin/sudo /usr/bin/xdg-user-dirs-update
 }
 
-desktop() {
-    # Copy desktop specific dotfiles to $USER's home directory:
-    /usr/bin/cp -r "$1"/Desktop/etc/skel/. "$HOME"/
-    /usr/bin/cp -r "$1"/etc/skel/Pictures/ "$HOME"/
-
-    # Copy desktop specific dotfiles to root's home directory:
-    /usr/bin/sudo /usr/bin/cp -r "$1"/Desktop/etc/skel/. /root/
-    /usr/bin/sudo /usr/bin/cp -r "$1"/etc/skel/Pictures/ /root/
-
-    # Replace mentions of $HOME in nitrogen dotfiles to root:
-    /usr/bin/sudo /usr/bin/sed -i "s|$HOME|\/root|g" /root/.config/nitrogen/bg-saved.cfg
-    /usr/bin/sudo /usr/bin/sed -i "s|$HOME|\/root|g" /root/.config/nitrogen/nitrogen.cfg
-
-}
-
-laptop() {
-    # Copy desktop specific dotfiles to $USER's home directory:
-    /usr/bin/cp -r "$1"/Laptop/etc/skel/. "$HOME"/
-    /usr/bin/mkdir -p "$HOME"/Pictures/Wallpapers && /usr/bin/cp "$1"/etc/skel/Pictures/Wallpapers/horizontal.png "$_"
-
-    # Copy desktop specific dotfiles to root's home directory:
-    /usr/bin/sudo /usr/bin/cp -r "$1"/Laptop/etc/skel/. /root/
-    /usr/bin/sudo /usr/bin/mkdir -p /root/Pictures/Wallpapers && /usr/bin/sudo /usr/bin/cp "$1"/etc/skel/Pictures/Wallpapers/vertical.png "$_"
-
-    # Replace mentions of $HOME in nitrogen dotfiles to root:
-    /usr/bin/sudo /usr/bin/sed -i "s|$HOME|\/root|g" /root/.config/nitrogen/bg-saved.cfg
-    /usr/bin/sudo /usr/bin/sed -i "s|$HOME|\/root|g" /root/.config/nitrogen/nitrogen.cfg
-}
-
 ohmyzsh() {
     # Install ohmyzsh for $USER:
-    /usr/bin/sh -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    /usr/bin/sh -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ""
 
     # Install zsh-autosuggestions for $USER:
     /usr/bin/git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
@@ -52,7 +19,7 @@ ohmyzsh() {
     /usr/bin/git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-completions
 
     # Install ohmyzsh for root:
-    /usr/bin/sudo /usr/bin/sh -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    /usr/bin/sudo /usr/bin/sh -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ""
 
     # Install zsh-autosuggestions for root:
     /usr/bin/sudo /usr/bin/git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-/root/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
@@ -62,57 +29,19 @@ ohmyzsh() {
 }
 
 general() {
-    # Copy general dotfiles (excluding .mozilla/) to $USER's home directory:
-    /usr/bin/rsync -az --exclude .mozilla --exclude Pictures/ "$1"/etc/skel/ "$HOME"/
+    # Copy `.zshrc` to $USER's home directory:
+    /usr/bin/cp ./etc/skel/.zshrc "$HOME"/
 
     # Copy `userChrome.css` needed for Tree Style Tab plugin:
-    /usr/bin/cp -r "$1"/etc/skel/.mozilla/. "$HOME"/.mozilla/firefox/*.default-esr/
+    /usr/bin/cp -r ./etc/skel/.mozilla/. "$HOME"/.mozilla/firefox/*.default-esr/
 
-    # Copy general dotfiles (excluding .mozilla/) to root's home directory:
-    /usr/bin/sudo /usr/bin/rsync -az --exclude .mozilla --exclude Pictures/ "$1"/etc/skel/ /root/
+    # Copy `.zshrc` to root's home directory:
+    /usr/bin/cp ./etc/skel/.zshrc /root/
 
     # Update .zshrc's ZSH variable from $USER to root:
-    /usr/bin/sudo /usr/bin/sed -i "s|$HOME|\/root|g" /root/.zshrc
-
-    # Copy ssh_config to /etc/ssh/:
-    /usr/bin/sudo /usr/bin/cp "$1"/etc/ssh/ssh_config /etc/ssh/
+    /usr/bin/sudo /usr/bin/sed -i "s|\$HOME|\/root|g" /root/.zshrc
 }
 
-main() {
-    # We're going to need root rights at some point:
-    if [ "$(whoami)" != "root" ]; then
-        /usr/bin/sudo -v
-    fi
-
-    # Check if any flags are given, if not display the help menu
-    if (($# != 1)); then
-        help_menu
-        exit 1
-    fi
-
-    # Get CWD i.e. where the script is located:
-    script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/" && pwd)"
-
-    while getopts ":dlh" option; do
-        case $option in
-        d) # Desktop
-            user_dirs
-            desktop "$script_dir"
-            ohmyzsh
-            general "$script_dir"
-            ;;
-        l) # Laptop
-            user_dirs
-            laptop "$script_dir"
-            ohmyzsh
-            general "$script_dir"
-            ;;
-        h | \? | *) # Display help
-            help_menu
-            exit 1
-            ;;
-        esac
-    done
-}
-
-main "$@"
+user_dirs
+ohmyzsh
+general
